ce13395221f3bea7765bb0d7030e4206fc6a6b1f
Nileshhansley10 <hansleybapamah10@gmail.com>
2023-04-24 23:48:25 +0400

    First commit

---
+++ Week2/Day 1/Daily Challenge/index.js
@@ -0,0 +1,25 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+console.log(fruits);
+
+fruits.sort()
+console.log(fruits);
+
+fruits.push('kiwi');
+console.log(fruits);
+
+fruits.splice(0,1);
+console.log(fruits);
+
+fruits.reverse();
+console.log(fruits);
+
+
+// ex2
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0])
+
+
+
---
+++ Week2/Day 1/Exercise/index.js
@@ -0,0 +1,26 @@
+// ex1
+
+let favoriteFood= "Pizza";
+let favoriteMeal="dinner";
+
+console.log("I eat" + " " + favoriteFood  + " at every"+ " " +favoriteMeal);
+
+// ex2
+
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length;
+console.log(myWatchedSeriesLength);
+
+
+let myWatchedSeriesSentence=["black mirror", "money heist", and "the big bang theory"];
+let myWatchedSeriesSentence=  myWatchedSeries[0] ", " +  myWatchedSeries[1] " ", + and + myWatchedSeries[2] " " 
+
+console.log(myWatchedSeriesSentence);
+
+
+
+
+
+
+
+ 
